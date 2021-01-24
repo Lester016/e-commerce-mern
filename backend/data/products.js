e861ccf1e7c47ac667a5efb9c9d6c4d1bf18c70c
Lester016 <lesterdeguzman1999@gmail.com>
2021-01-24 13:32:08 +0800

    Initialize app

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    name: "Mayonnais Eggless",
+    image: "/images/prod1.png",
+    description:
+      "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+    brand: "Nestle",
+    category: "Food",
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Egg Noodles",
+    image: "/images/prod2.png",
+    description: "180g, Price",
+    brand: "Nestle",
+    category: "Food",
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Egg Pasta",
+    image: "/images/prod3.png",
+    description:
+      "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+    brand: "Nestle",
+    category: "Food",
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Egg Chicken Red",
+    image: "/images/prod4.png",
+    description:
+      "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+    brand: "Nestle",
+    category: "Food",
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Egg Chicken White",
+    image: "/images/prod5.png",
+    description:
+      "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+    brand: "Nestle",
+    category: "Food",
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Egg Noodles",
+    image: "/images/prod6.png",
+    description:
+      "Apples are nutritious. Apples may be good for weight loss. apples may be good for your heart. As part of a healtful and varied diet.",
+    brand: "Nestle",
+    category: "Food",
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Welcome To Lester Store",
+  description: "We sell the best products for cheap",
+  keywords: "Foods, buy foods, cheap foods",
+};
+
+export default Meta;
